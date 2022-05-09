0b1a7f035096cb6d0bf79d0389dd1a3b2d21402a
Maria Bystrova <mariabystrova@MacBook-Pro-Maria.local>
2022-05-09 13:31:29 +0300

    Fix toast coating

--- src/test/java/ru/netology/radio/RadioTest.java
+++ src/test/java/ru/netology/radio/RadioTest.java
@@ -17,6 +17,7 @@ class RadioTest {
         assertEquals(expected, actual);
     }
 
+
     @Test
     void shouldShowTheCurrentNamber() {
         Radio radio = new Radio();
@@ -27,8 +28,33 @@ class RadioTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void shouldShowTheCurrentNamberReturn() {
+        Radio radio = new Radio();
+        radio.setStation(10);
+
+        int expected = 0;
+        int actual = radio.getStation();
+
+        assertEquals(expected, actual);
+    }
+
     @Test
     void shouldShowNextTheNamber() {
+        Radio radio = new Radio();
+        radio.setStation(7);
+
+        radio.next();
+
+        int expected = 8;
+        int actual = radio.getStation();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldShowNextTheNamberFulfillingTheCondition() {
         Radio radio = new Radio();
         radio.setStation(9);
 
@@ -39,8 +65,23 @@ class RadioTest {
 
         assertEquals(expected, actual);
     }
+
     @Test
-    void shouldShowPrevTheNamber() {
+    void shouldShowPrevThe() {
+        Radio radio = new Radio();
+        radio.setStation(6);
+
+        radio.prev();
+
+        int expected = 5;
+        int actual = radio.getStation();
+
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    void shouldShowPrevTheNamberFulfillingTheCondition() {
         Radio radio = new Radio();
         radio.setStation(0);
 
@@ -51,8 +92,6 @@ class RadioTest {
 
         assertEquals(expected, actual);
     }
-
-
 
     @Test
     void shouldShowVolumeLevel() {
@@ -75,8 +114,33 @@ class RadioTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void shouldShowTheCurrentVolumeFulfillingTheCondition() {
+        Radio radio = new Radio();
+        radio.setVolume(11);
+
+        int expected = 0;
+        int actual = radio.getVolume();
+
+        assertEquals(expected, actual);
+    }
+
     @Test
     void shouldShowVolumeUp() {
+        Radio radio = new Radio();
+        radio.setVolume(3);
+
+        radio.volumeUp();
+
+        int expected = 4;
+        int actual = radio.getVolume();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldShowVolumeUpFulfillingTheCondition() {
         Radio radio = new Radio();
         radio.setVolume(10);
 
@@ -87,8 +151,22 @@ class RadioTest {
 
         assertEquals(expected, actual);
     }
+
     @Test
     void shouldShowVolumeDowne() {
+        Radio radio = new Radio();
+        radio.setVolume(6);
+
+        radio.volumeDown();
+
+        int expected = 5;
+        int actual = radio.getVolume();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldShowVolumeDowneFulfillingTheCondition() {
         Radio radio = new Radio();
         radio.setVolume(10);
 
